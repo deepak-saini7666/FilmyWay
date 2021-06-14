9d8a6cd7b445467574fbc77aad9643d6cff8a191
Deepak <deepsaini203@gmail.com>
2021-06-14 17:58:18 +0530

    Initial Commit

---
+++ src/components/MainComponent.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import '../App.css';
+import Header from './HeaderComponent';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import {MOVIES} from './movies';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+
+class Main extends Component {
+  
+  constructor(props) {
+    super(props);
+     this.state = {
+         movies: MOVIES,
+         filter: " "
+     }
+  }
+
+  handleChange=(event) => {
+     this.setState({
+         filter: event.target.value
+     })
+  }
+     
+  render(){
+        
+      const lowerCaseFilter=this.state.filter.toLocaleLowerCase()
+      const list = this.state.movies.filter((val) => {
+         return Object.keys(val).some(key=>{
+           if(typeof val[key]==='string'){
+               return val[key].toLocaleLowerCase().includes(lowerCaseFilter)
+           }
+         })
+      }).map((movie) => {
+            return (
+              <div  className="col-12 col-md-2 m-2">
+                <Card key={movie.id}>
+                  <CardImg width="100%" height="250px" src={movie.image} alt={movie.name} />
+                      <CardTitle>{movie.name}</CardTitle>
+                </Card>
+              </div>
+            );
+        });
+
+    return (
+      <div>
+              <Header/>
+              <Switch>
+              <Route path='/home'  />
+              <Route path='/aboutus' />
+              <Route exact path='/menu' />
+              <Route path='/menu/:dishId' />
+              <Route exact path='/contactus' />
+              <Redirect to="/home" />
+              </Switch>
+              <div className="icon">
+              <input type="text" placeholder="Search..."
+              onChange={(event) => this.handleChange(event)}/>
+              </div>
+              <div className="container">
+              <div className="row">
+                  {list}
+              </div>
+              </div>
+      </div>
+    );
+ }
+}
+export default Main;
---
+++ src/components/movies.js
@@ -0,0 +1,53 @@
+export const MOVIES = 
+  [
+    {
+      id: 0,
+      name:'300',
+      image: '/assets/posters/300.jpg'
+    },
+    {
+      id: 1,
+      name:'Avengers: Endgame',
+      image: '/assets/posters/Avengers.jpg'
+    },
+    {
+      id: 2,
+      name:'Game of Thrones',
+      image: '/assets/posters/GOT.jpg'
+    },
+    {
+      id: 3,
+      name:'Inception',
+      image: '/assets/posters/Inception.jpg'
+    },
+    {
+      id: 4,
+      name:'Iron Man',
+      image: '/assets/posters/IronMan.jpg'
+    },
+    {
+      id: 5,
+      name:'Mission Impossible',
+      image: '/assets/posters/MI.jpg'
+    },
+    {
+      id: 6,
+      name:'Kung Fu Hustle',
+      image: '/assets/posters/KFH.jpg'
+    },
+    {
+      id: 7,
+      name:'The Wolverine',
+      image: '/assets/posters/Wolverine.jpg'
+    },
+    {
+      id: 8,
+      name:'Friends',
+      image: '/assets/posters/Friends.jpg'
+    },
+    {
+      id: 9,
+      name:'XXX',
+      image: '/assets/posters/Xxx.jpg'
+    },
+  ];
